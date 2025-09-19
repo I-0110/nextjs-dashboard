ce85fe6a1cbe80ae3a77cabb34a0703b4246e1be
IvelisBecker <ivelisbecker@gmail.com>
2025-09-19 16:44:59 -0500

    Added styles and dashboard

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,13 +1,13 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import { MusicalNoteIcon } from '@heroicons/react/24/outline';
+import { josefin } from '@/app/ui/fonts';
 
 export default function AcmeLogo() {
   return (
     <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+      className={`${josefin.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      <MusicalNoteIcon className="h-12 w-12" />
+      <p className="text-[24px] w-2">Music Time!</p>
     </div>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Teachers, Josefin_Sans } from 'next/font/google';
+
+// change lusitana for teachers
+export const teachers = Teachers({ subsets: ['latin'] });
+
+// change inter for josefin
+export const josefin = Josefin_Sans({ subsets: ['latin'] });
