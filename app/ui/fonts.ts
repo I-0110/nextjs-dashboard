b251a6667ea099734b164c2319ab20acf1fa5c9f
IvelisBecker <ivelisbecker@gmail.com>
2025-09-19 19:39:33 -0500

    Dashboard page almost fully functional

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,10 @@
 import { Lusitana, Inter } from 'next/font/google';
 
 // change lusitana for teachers
-export const lusitana = Lusitana({ subsets: ['latin'], weight: "400" });
+export const lusitana = Lusitana({ 
+    subsets: ['latin'], 
+    weight: ['400', '700'],
+ });
 
 // change inter for josefin
 export const inter = Inter({ subsets: ['latin'] });
